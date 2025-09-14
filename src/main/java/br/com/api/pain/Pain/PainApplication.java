70434118427d491ad03d4f11809aefcc03b29e7e
mateus.rocha <mateusrochagoncalves@gmail.com>
2025-09-14 14:26:22 -0300

    Commit inicial

---
+++ src/main/java/br/com/api/pain/Pain/PainApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.pain.Pain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/pain/Pain/PainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.pain.Pain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
